e6e1f130d84a596e598f19455c1756b5e590dffe
HimeragiYukina <121090410@link.cuhk.edu.cn>
2023-11-30 11:35:39 +0800

    11/30 (2)

--- HomePage.Designer.cs
+++ HomePage.Designer.cs
@@ -34,13 +34,16 @@ namespace LibrarySystem
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.searchBox = new LibrarySystem.SearchBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(89, 77);
+            this.button1.Location = new System.Drawing.Point(5, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 64);
+            this.button1.Size = new System.Drawing.Size(250, 250);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,52 +51,67 @@ namespace LibrarySystem
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(257, 77);
+            this.button2.Location = new System.Drawing.Point(5, 262);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(101, 63);
+            this.button2.Size = new System.Drawing.Size(250, 250);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(89, 174);
+            this.button3.Location = new System.Drawing.Point(5, 520);
+            this.button3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(97, 66);
+            this.button3.Size = new System.Drawing.Size(250, 250);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(257, 174);
+            this.button4.Location = new System.Drawing.Point(5, 778);
+            this.button4.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(101, 65);
+            this.button4.Size = new System.Drawing.Size(250, 250);
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // searchBox
             // 
-            this.searchBox.Location = new System.Drawing.Point(61, 288);
+            this.searchBox.Location = new System.Drawing.Point(545, 253);
+            this.searchBox.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
             this.searchBox.Name = "searchBox";
-            this.searchBox.Size = new System.Drawing.Size(513, 81);
+            this.searchBox.Size = new System.Drawing.Size(798, 122);
             this.searchBox.TabIndex = 4;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(338, 1104);
+            this.flowLayoutPanel1.TabIndex = 5;
             // 
             // HomePage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(635, 448);
+            this.ClientSize = new System.Drawing.Size(1852, 1104);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.searchBox);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "HomePage";
             this.Text = "HomePage";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.HomePage_FormClosing);
             this.Load += new System.EventHandler(this.HomPage_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -105,5 +123,6 @@ namespace LibrarySystem
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private SearchBox searchBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
